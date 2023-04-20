f1efe6dc1877d0fd574e97f84d1e1e6b7772d976
tkolsrud <thomaskolsrud@gmail.com>
2023-04-20 15:08:57 -0700

    complete authorization lesson pt5

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,12 +1,18 @@
 const Movie = require('../models/movie');
 
 module.exports = {
-  create
+  create,
+  deleteReview
 };
 
 async function create(req, res) {
   const movie = await Movie.findById(req.params.id);
   // We can push (or unshift) subdocs into Mongoose arrays
+   // Add the user-centric info to req.body (the new review)
+  req.body.user = req.user._id;
+  req.body.userName = req.user.name;
+  req.body.userAvatar = req.user.avatar;
+   
   movie.reviews.push(req.body);
   try {
     // Save any changes made to the movie doc
@@ -16,4 +22,26 @@ async function create(req, res) {
   }
   // Step 5:  Respond to the Request (redirect if data has been changed)
   res.redirect(`/movies/${movie._id}`);
+}
+
+// controllers/reviews.js
+
+// Include the next parameter - used for error handling in the catch
+function deleteReview(req, res, next) {
+  // Note the cool "dot" syntax to query on the property of a subdoc
+  Movie.findOne({'reviews._id': req.params.id, 'reviews.user': req.user._id}).then(function(movie) {
+    // Rogue user!
+    if (!movie) return res.redirect('/movies');
+    // Remove the review using the remove method available on Mongoose arrays
+    movie.reviews.remove(req.params.id);
+    // Save the updated movie
+    movie.save().then(function() {
+      // Redirect back to the movie's show view
+      res.redirect(`/movies/${movie._id}`);
+    }).catch(function(err) {
+      // Let Express display an error
+      return next(err);
+      // res.redirect(`/movies/${movie._id}`);
+    });
+  });
 }
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 const passport = require('passport');
+var methodOverride = require('method-override')
 
 require('dotenv').config();
 // connect to the database with AFTER the config vars are processed
@@ -27,6 +28,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 app.use(session({
   secret: process.env.SECRET,
   resave: false,
